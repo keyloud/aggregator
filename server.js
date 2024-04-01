07ba4ba0cf60c5e193b407256714c23114d7b789
xarubri <ivan_iershov@mail.ru>
2024-04-02 03:49:17 +0400

    исправил ошибку в массиве, из-за которой не выводилась информация о пользователе

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get("/usr_profile/:registrations_id", checkUser, function (req, res) {
     }
     //Если юзер авторизирован, то покажет страницу, если нет ,то err
     if (req.session.user) {
-      res.render("usr_profile", { customer: data });
+      res.render("usr_profile", { customer: data[0] });
     } else {
       res.status(401).send('Необходима аутентификация');
     }
@@ -310,10 +310,10 @@ app.post('/usr_registration', (req, res) => {
     }
 
     // Сохранение хеша пароля и остальных данных в базе данных
-    const queryUsr = 'INSERT INTO customer (customer_name, customer_surname, customer_patronymic, customer_phone_number, customer_email, add_info ) VALUES (?, ?, ?, ?, ?, ?)';
+    const queryUsr = 'INSERT INTO customer (customer_name, customer_surname, customer_patronymic, customer_phone_number, customer_email, add_info, profile_image ) VALUES (?, ?, ?, ?, ?, ?, ?)';
     const queryReg = 'INSERT INTO registrations (email, password, type) VALUES (?, ?, ?)';
 
-    pool.query(queryUsr, [customer_name, customer_surname, customer_patronymic, customer_phone_number, customer_email, add_info], (err, result) => {
+    pool.query(queryUsr, [customer_name, customer_surname, customer_patronymic, customer_phone_number, customer_email, add_info, profile_image], (err, result) => {
       if (err) {
         console.error('Ошибка при добавлении пользователя в базу данных:', err);
         return res.status(500).send('Ошибка при регистрации пользователя');
